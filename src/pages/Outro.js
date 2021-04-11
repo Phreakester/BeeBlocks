4ab6001b4f81f191b7b95265f06bc30d96c84c2b
phreakester <gettyv05@gmail.com>
2021-04-11 10:21:34 -0700

    Changed the styling to scale better across screens

--- src/pages/Outro.js
+++ src/pages/Outro.js
@@ -7,7 +7,7 @@ import BckButton from '../shared/BckButton.js';
 function Outro(props) {
     return(
         <div className="main-outro">
-            <div className="outro-text">Bee Blocks: Because we BEElieve in making the world a better place</div>
+            <div className="outro-text">Thank you for taking the time to learn about Bee Blocks!</div>
             <a className='outro-button' target="_blank" href='https://www.youtube.com/watch?v=ZORzsubQA_M'>Learn more about superblocks</a>
             <a className='outro-button' target="_blank" href='https://www.youtube.com/watch?v=SyPjwxHxbus'>Learn more about bees</a>
             <a className='outro-button' target="_blank" href='https://docs.google.com/presentation/d/1yOINC_jJQQV65akMZLFP2bwIvDO9V3kMZJLUqs0RL-0/edit?usp=sharing'>View our presentation Deck to gain insight as to our creative process</a>
--- src/pages/Superblock.js
+++ src/pages/Superblock.js
@@ -8,7 +8,7 @@ import BckButton from '../shared/BckButton.js';
 
 import red_triangle from './../assets/red_triangle.svg';
 
-const default_text = 'Hover over one of the areas on the map to learn more about how superblocks can utilize space more efficiently and reduce traffic congestion!'
+const default_text = 'Hover over one of the areas on the map to learn more about how BeeBlocks can utilize space more efficiently and reduce traffic congestion!'
 const inner_road_text = 'The lanes originally used for driving are converted into a community space. Between the buildings, walkable pathways will be constructed, encouraging pedestrians.'
 const outer_road_text = 'Traffic is directed around the perimeter of the BeeBlocks, decreasing the level of noise pollution within the complex.'
 const bus_text = 'Public transit is located on the outside streets surrounding the BeeBlocks. These bus stops are strategically placed along the edge of each superblock, making public transportation an efficient and accessible option for residents.'
@@ -19,7 +19,7 @@ function Superblock(props) {
     const [currentText, setCurrentText] = useState(default_text);
     return(
         <div className="main-superblock">
-            <div className='header-text'><b>Welcome to your new Super Block!</b></div>
+            <div className='header-text'><b>Welcome to your new BeeBlock!</b></div>
             <div className="info-text">{currentText}</div>
             <Hoverable src={red_triangle} className='hover-inner-road' setCurrentText={setCurrentText} text={inner_road_text} defaultText={default_text} />
             <Hoverable src={red_triangle} className='hover-outer-road' setCurrentText={setCurrentText} text={outer_road_text} defaultText={default_text} />
