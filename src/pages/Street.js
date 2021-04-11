aaab72664d8933d54fe7755ea034673ecbf41592
phreakester <gettyv05@gmail.com>
2021-04-10 23:25:23 -0700

    Prepped street for final image

--- src/pages/Street.js
+++ src/pages/Street.js
@@ -8,7 +8,12 @@ import BckButton from '../shared/BckButton.js';
 
 import red_triangle from './../assets/red_triangle.svg';
 
-const planter_text = 'This is a planter for plants';
+const garden_text = 'The gardens bring much needed green spaces to urban living, increasing the quality of life for the residents. Native plants would be the focus of these gardens, as these species have been displaced by the city.';
+const sidewalk_text = 'The streets are converted to widened sidewalks, allowing for pedestrians to walk to their local shops instead of driving there and reducing traffic flow in the area. Also, added bike lanes encourage biking as an alternative to driving. The lack of cars in this area creates increased safety for pedestrian and biking commuters. Air and noise pollution will see a marked decrease as well.';
+const bee_text = 'It is well known that the increase of urban sprawl has decreased the bee population significantly due to the lack of green spaces. This issue is our inspiration for the name of this project, as one small part of the BeeBlock system would be a BeeBox on one of the roofs of the complex. This BeeBox would bring the bees back to the cities and to these communities to foster the growth of the native plant species.';
+const market_text = 'Markets and everyday essentials will be located within these BeeBlocks. Biking and walking will be viable options to reach these destinations, reducing the need for cars thus improving air quality.';
+const shade_text = 'A projected increase in global temperature will require new methods of cooling down the city. A shaded canopy over a part of the complex would reduce the solar impact on city temperatures and improve walkability for pedestrians within the BeeBlocks.';
+
 const default_text = 'This is the default text'
 
 function Street(props) {
@@ -16,7 +21,11 @@ function Street(props) {
     return(
         <div className='main-street'>
             <div className="infro-text">{currentText}</div>
-            <Hoverable src={red_triangle} className='hover-planter' setCurrentText={setCurrentText} text={planter_text} defaultText={default_text} />
+            <Hoverable src={red_triangle} className='hover-garden' setCurrentText={setCurrentText} text={garden_text} defaultText={default_text} />
+            <Hoverable src={red_triangle} className='hover-sidewalk' setCurrentText={setCurrentText} text={sidewalk_text} defaultText={default_text} />
+            <Hoverable src={red_triangle} className='hover-bee' setCurrentText={setCurrentText} text={bee_text} defaultText={default_text} />
+            <Hoverable src={red_triangle} className='hover-market' setCurrentText={setCurrentText} text={market_text} defaultText={default_text} />
+            <Hoverable src={red_triangle} className='hover-shade' setCurrentText={setCurrentText} text={shade_text} defaultText={default_text} />
             <div className="button-container">
                 <BckButton destination={props.prevPath} />
                 <FwdButton destination={props.nextPath} />
