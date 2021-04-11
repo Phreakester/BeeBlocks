bbfc07992a5d4f85c9c14a033baba82bc5480fc1
phreakester <gettyv05@gmail.com>
2021-04-10 18:34:39 -0700

    Added much of the styling for the intro slide as well as some setup for the future hoverable elements

--- src/pages/Superblock.js
+++ src/pages/Superblock.js
@@ -1,16 +1,22 @@
 import './../styles/Superblock.css';
 
+import { useState } from 'react';
+
+import Hoverable from './../shared/Hoverable.js';
+
 import background from './../assets/superblock.svg';
+import red_triangle from './../assets/red_triangle.svg';
+
+const default_text = 'Hover over one of the areas on the map to learn more about how superblocks can utilize space more efficiently and reduce traffic congestion!'
+const grocer_text = 'This is the grocer. Not  grocery s that would be harder to type'
+
 
 function Superblock() {
+    const [currentText, setCurrentText] = useState(default_text);
     return(
         <div className="main-superblock">
-            <img src={background} alt="Workplace" useMap="#workmap" />
-            <map name="workmap">
-                <area className="grocer-hover" shape="rect" coords="34,44,270,350" alt="Computer" href="computer.htm" />
-                <area shape="rect" coords="290,172,333,250" alt="Phone" href="phone.htm" />
-                <area shape="circle" coords="337,300,44" alt="Cup of coffee" href="coffee.htm" />
-            </map>
+            <div>{currentText}</div>
+            <Hoverable src={red_triangle} className='hover-grocer' setCurrentText={setCurrentText} text={grocer_text} defaultText={default_text} />
         </div>
     );
 }
